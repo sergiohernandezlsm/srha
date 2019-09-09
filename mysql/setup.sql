cdccf82f433558d7e276fd01e6c4e79c1d40d410
Sergio  Hernandez <sergiohernandez@sergios-mbp.lsm.local>
2019-09-09 13:52:18 +0100

    docker mysql

---
+++ mysql/setup.sql
@@ -0,0 +1,7 @@
+/*
+SQLyog Professional v12.09 (64 bit)
+MySQL - 5.7.17-0ubuntu0.16.04.1 : Database - cg_apiserver
+*********************************************************************
+*/
+
+CREATE DATABASE IF NOT EXISTS srha_local;
